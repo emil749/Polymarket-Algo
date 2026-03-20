af6ecb2e7411c785f9c56508154e8b7df1ea22b1
emil749 <emilbosse@gmail.com>
2026-03-20 21:34:14 +0100

    Update server.py

--- server.py
+++ server.py
@@ -274,4 +274,5 @@ if __name__ == "__main__":
     # Kurz warten damit erster Update durch ist
     time.sleep(2)
 
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=False)
